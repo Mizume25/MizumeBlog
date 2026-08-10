825a885587003853f836da4b8adce48450f6eadf
Mizume25 <gabrielnivicela649@gmail.com>
2026-08-10 17:35:53 +0200

    Migracion a terminal linux

--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,7 @@ export default defineConfig({
         include: ['@headlessui/react'],
     },
     server: {
+	cors: true,
         host: '0.0.0.0',
         port: 5173,
         strictPort: true,
