c4031f881a8e4fd7b638aee24df6a9f70dd9d206
Mizume25 <gabrielnivicela649@gmail.com>
2026-09-01 16:01:04 +0200

    Borrado de builds

--- public/build/assets/interfaces-BT4iri12.js
+++
@@ -1,1 +0,0 @@
-const t={home:"center",article:{position:"center",height:"35vh"}},e=["top","center","bottom"],o=["top left","top center","top right","center left","center","center center","center right","bottom left","bottom center","bottom right"];export{e as B,o as a,t as f};
